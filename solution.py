e3debfb43cffa18bcb9e3a85399f6d2745a975c2
immortallz <129889191+immortallz@users.noreply.github.com>
2023-04-13 17:32:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,7 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    if proportions_ztest([x_success,y_success],[x_cnt,y_cnt], alternative = 'larger')[1] < 0.1:
+    if proportions_ztest([x_success,y_success],[x_cnt,y_cnt], alternative = 'larger')[1] > 0.1:
       return False
     else:
       return True
